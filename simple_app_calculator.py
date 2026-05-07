a338ec1de9a656eac24e2b5fe9d639135435f615
Kimberly Alvarado <kimmyjugado@gmail.com>
2026-05-08 01:00:19 +0800

    Continuation of the program (2)

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -33,7 +33,7 @@ class KimmyCalculatorApp:
         tk.Label(self.root, text="Choose Operation: ", bg="yellow").pack(pady=5)
         self.operation_var = tk.StringVar(root)
         self.operation_var.set("Select Operation")
-        options = ["Addition", "Subtraction", "Multiplication", "Division", "Power", "Square Root"]
+        options = ["Addition", "Subtraction", "Multiplication", "Division", "Exponent", "Square Root"]
         self.dropdown = tk.OptionMenu(root, self.operation_var, *options)
         self.dropdown.pack()
 
@@ -43,10 +43,37 @@ class KimmyCalculatorApp:
         self.label_result.pack(pady=10)
 
         self.btn_reset = tk.Button(self.root, text="Try Again", command=self.reset_program, bg="white", fg="green", font=("Times New Roman", 12, "bold"))
-        self.btn_reset.pack(pady=5)
+        self.btn_reset.pack(pady=7)
 
         self.btn_exit = tk.Button(self.root, text="Exit", command=self.exit_program, bg="white", fg="red")
-        self.btn_exit.pack(pady=5)
+        self.btn_exit.pack(pady=7)
+
+    def calculate(self):
+        try:
+
+
+
+           if operation == "Addition":
+               result = self.add(num1, num2)
+           elif operation == "Subtraction":
+               result = self.subtract(num1, num2)
+           elif operation == "Multiplication":
+                result = self.multiply(num1, num2)
+           elif operation == "Division":
+               result == self.divide(num1, num2)
+           elif operation == "Power":
+               result = self.power(num1, num2)
+           elif operation == "Square Root":
+               result = self.square_root(num1, num2)
+           else:
+               messagebox.showeeror(title= "Invalid Input", message= "Input Value is not recognized.")
+               return
+
+           self.label_result.config(text=f"Result: {result}", fg="blue")
+
+
+
+
 
 
 
